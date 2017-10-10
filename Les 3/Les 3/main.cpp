f112dc4e19d358373beea3edf1ee30b2e05ed8a9
Michael Sanstra <michaelsanstra@gmail.com>
2017-10-10 10:29:28 +0200

    Laatste les

---
+++ Les 3/Les 3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Les 3
+//
+//  Created by Michael Sanstra on 04/10/2017.
+//  Copyright © 2017 Michael Sanstra. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
